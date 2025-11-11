7976c8499dcb7507b1a10a633d3686483f47445a
doaahasham <doohesham452@gmail.com>
2025-11-11 15:18:50 +0200

    Middleware+Models

--- MiddleWares/checklogin.js
+++ MiddleWares/checklogin.js
@@ -1,1 +1,13 @@
 // auth 
+const checkLogin = (req, res, next) => {
+  const token = req.header("Authorization" 
+    || "authorization")?.split(" ")[1]; 
+  if (!token) {
+     return res.status(400).json({
+       message: "not allowed",
+        data: null,
+       });
+       } 
+       next();
+       };
+module.exports=checkLogin;
--- Routes/cars.js
+++ Routes/cars.js
@@ -1,1 +1,7 @@
 // car routes
+const express = require("express");
+
+const checkLogin = require("../MiddleWares/checklogin");
+const mongoose = require("mongoose");
+const router = express.Router();
+module.exports = router;
