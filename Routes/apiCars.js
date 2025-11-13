46e830a764040a6dd88050109bee4553cc44eab4
mariammaahmoud <merofooly@gmail.com>
2025-11-13 03:43:21 +0200

    addNewCar,editCar,deleteCar

--- Routes/apiCars.js
+++ Routes/apiCars.js
@@ -3,9 +3,14 @@ const router = express.Router();
 
 const getAllCars = require("../Controllers/Cars/getAllCars");
 const getSingleCar = require("../Controllers/Cars/getSingleCar");
+const addNewCar = require("../Controllers/Cars/addNewCar");
+const editCar = require("../Controllers/Cars/editCar");
+const deleteCar = require("../Controllers/Cars/deleteCar");
 
 router.get("/", getAllCars);
-
 router.get("/:id", getSingleCar);
+router.post("/", addNewCar);
+router.put("/:id", editCar);
+router.delete("/:id", deleteCar);
 
 module.exports = router;
